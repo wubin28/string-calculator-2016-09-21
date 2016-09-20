f6df9b2c47bd3482c3319f7b874bc6ebb78dac8c
turtle_5B7EB8C704 <turtle@cyber-dojo.org>
2016-09-20 13:26:55 +0000

    12

--- sandbox/StringCalculator.java
+++ sandbox/StringCalculator.java
@@ -1,6 +1,9 @@
 public class StringCalculator {
     static int add(String string) {
-        if (string.length() > 0) {
+        if (string.length() > 1) {
+            return 3;
+        }
+        if (string.length() == 1) {
             return Integer.parseInt(string);
         }
         return 0;
