1f3f22ef0cfa45528b97342ae09ef86887b8b25b
turtle_5B7EB8C704 <turtle@cyber-dojo.org>
2016-09-20 15:06:55 +0000

    39

--- sandbox/ParserWithCustomizedDelimiter.java
+++ sandbox/ParserWithCustomizedDelimiter.java
@@ -1,2 +1,5 @@
 public class ParserWithCustomizedDelimiter implements StringCalculatorParser {
+    List<String> parse(String string) {
+        return Arrays.asList(string.substring(4).split("\\s*" + string.substring(2, 3) + "\\s*"));
+    }
 }
