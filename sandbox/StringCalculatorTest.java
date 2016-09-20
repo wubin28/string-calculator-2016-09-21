851c8e61fc80a1dd7591f3c5f64cb8b2fd392afd
turtle_5B7EB8C704 <turtle@cyber-dojo.org>
2016-09-20 14:58:12 +0000

    34

--- sandbox/StringCalculatorTest.java
+++ sandbox/StringCalculatorTest.java
@@ -10,22 +10,30 @@ public class StringCalculatorTest {
 
     @Test
     public void the_result_of_one_number_is_itself() {
-        assertEquals(1, StringCalculator.add("1"));
+        StringCalculatorParser parserWithDelimiterNewLineOrComma =
+            new ParserWithDelimiterNewLineOrComma();
+        assertEquals(1, StringCalculator.add("1", parserWithDelimiterNewLineOrComma));
     }
 
     @Test
     public void the_result_of_two_numbers_separated_by_a_comma_is_the_sum_of_them() {
-        assertEquals(3, StringCalculator.add("1,2"));
+        StringCalculatorParser parserWithDelimiterNewLineOrComma =
+            new ParserWithDelimiterNewLineOrComma();
+        assertEquals(3, StringCalculator.add("1,2", parserWithDelimiterNewLineOrComma));
     }
 
     @Test
     public void the_result_of_unknown_amount_of_numbers_separated_by_commas_is_the_sum_of_them() {
-        assertEquals(6, StringCalculator.add("1,2,3"));
+        StringCalculatorParser parserWithDelimiterNewLineOrComma =
+            new ParserWithDelimiterNewLineOrComma();
+        assertEquals(6, StringCalculator.add("1,2,3", parserWithDelimiterNewLineOrComma));
     }
 
     @Test
     public void should_use_newlines_between_numbers_in_addition_to_commas() {
-        assertEquals(6, StringCalculator.add("1\n2,3"));
+        StringCalculatorParser parserWithDelimiterNewLineOrComma =
+            new ParserWithDelimiterNewLineOrComma();
+        assertEquals(6, StringCalculator.add("1\n2,3", parserWithDelimiterNewLineOrComma));
     }
 
     @Test
