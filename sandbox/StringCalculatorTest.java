36dfd40bfa1df28930ff6ad8cdebc1e4cc8eed78
turtle_5B7EB8C704 <turtle@cyber-dojo.org>
2016-09-20 13:20:59 +0000

    11

--- sandbox/StringCalculatorTest.java
+++ sandbox/StringCalculatorTest.java
@@ -12,4 +12,11 @@ public class StringCalculatorTest {
     public void the_result_of_one_number_is_itself() {
         assertEquals(1, StringCalculator.add("1"));
     }
+
+    @Test
+    public void the_result_of_two_numbers_separated_by_a_comma_is_the_sum_of_them() {
+        assertEquals(3, StringCalculator.add("1,2"));
+    }
 }
+
+
