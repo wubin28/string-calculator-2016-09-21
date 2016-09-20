061b182f815401fd581ae6d5f834db7fe4034099
turtle_5B7EB8C704 <turtle@cyber-dojo.org>
2016-09-20 13:13:11 +0000

    7

--- sandbox/StringCalculatorTest.java
+++ sandbox/StringCalculatorTest.java
@@ -7,4 +7,9 @@ public class StringCalculatorTest {
     public void the_result_of_empty_string_is_0() {
         assertEquals(0, StringCalculator.add(""));
     }
+
+    @Test
+    public void the_result_of_one_number_is_itself() {
+        assertEquals(1, StringCalculator.add("1");
+    }
 }
