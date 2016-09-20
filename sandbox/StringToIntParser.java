8930a6501aa7a910845e1998eb84402652f50414
turtle_5B7EB8C704 <turtle@cyber-dojo.org>
2016-09-20 14:19:23 +0000

    26

--- sandbox/StringToIntParser.java
+++ sandbox/StringToIntParser.java
@@ -3,6 +3,6 @@ import java.util.Arrays;
 
 public class StringToIntParser {
     static List<String> parse(String string) {
-        return Arrays.asList(string.split("\\s*,\\s*"));
+        return Arrays.asList(string.split("\\s*[,|\\n]\\s*"));
     }
 }
